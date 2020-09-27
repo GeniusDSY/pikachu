e41ef5156c28b42301033556647290e9afbb7592
Genius_DSY <genius_dsy@foxmail.com>
2020-09-27 17:48:39 +0800

    chore: init project

---
+++ src/test/java/cn/edu/cqupt/mislab/pikachu/PikachuApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.edu.cqupt.mislab.pikachu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PikachuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

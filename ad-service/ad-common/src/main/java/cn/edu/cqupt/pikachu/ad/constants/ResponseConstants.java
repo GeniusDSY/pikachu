a677f797449258073dd08483f2d7680fcdb9cde4
Genius_DSY <genius_dsy@foxmail.com>
2021-02-04 20:11:46 +0800

    :fire: feature:响应常量定义

---
+++ ad-service/ad-common/src/main/java/cn/edu/cqupt/pikachu/ad/constants/ResponseConstants.java
@@ -0,0 +1,15 @@
+package cn.edu.cqupt.pikachu.ad.constants;
+
+/**
+ * @author :DengSiYuan
+ * @date :2021/1/28 21:37
+ * @desc : 响应常量
+ */
+public interface ResponseConstants {
+
+    /**
+     * 空字符串
+     */
+    String EMPTY = "";
+
+}

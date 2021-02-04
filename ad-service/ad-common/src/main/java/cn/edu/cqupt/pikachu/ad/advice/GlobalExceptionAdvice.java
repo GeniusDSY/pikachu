1aa934aa9fdd7693e6941e5297b37840114b8270
Genius_DSY <genius_dsy@foxmail.com>
2021-02-04 20:00:56 +0800

    :fire: feature:统一异常处理

---
+++ ad-service/ad-common/src/main/java/cn/edu/cqupt/pikachu/ad/advice/GlobalExceptionAdvice.java
@@ -0,0 +1,35 @@
+package cn.edu.cqupt.pikachu.ad.advice;
+
+import cn.edu.cqupt.pikachu.ad.constants.enums.ResultStatus;
+import cn.edu.cqupt.pikachu.ad.exception.AdException;
+import cn.edu.cqupt.pikachu.ad.model.vo.response.Response;
+import cn.edu.cqupt.pikachu.ad.util.ResponseUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @author :DengSiYuan
+ * @date :2021/1/30 11:41
+ * @desc : 全局异常处理
+ */
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionAdvice {
+
+    /**
+     * 广告系统异常拦截处理方法
+     *
+     * @param request   Servlet请求
+     * @param exception 广告系统异常
+     * @return 异常处理响应结果
+     */
+    @ExceptionHandler({AdException.class})
+    public Response<Object> handlerAdException(HttpServletRequest request, AdException exception) {
+        log.error("Request：method = ", request.getMethod(), ". Exception = ", exception);
+        return ResponseUtils.fail(ResultStatus.SYSTEM_ERROR, exception.getMessage());
+    }
+
+}

3dc1eb4b0c8d04905b4af6702626a054eca8dd22
Genius_DSY <genius_dsy@foxmail.com>
2021-02-04 20:16:15 +0800

    :fire: feature: 广告系统异常自定义

---
+++ ad-service/ad-common/src/main/java/cn/edu/cqupt/pikachu/ad/exception/AdException.java
@@ -0,0 +1,14 @@
+package cn.edu.cqupt.pikachu.ad.exception;
+
+/**
+ * @author :DengSiYuan
+ * @date :2021/1/30 11:38
+ * @desc : 广告系统的自定义异常类
+ */
+public class AdException extends Exception {
+
+    public AdException(String message) {
+        super(message);
+    }
+
+}

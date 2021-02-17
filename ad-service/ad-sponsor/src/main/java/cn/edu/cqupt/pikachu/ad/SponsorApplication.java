b4d842a284dcd2f657de706484e7a5f6668f35f5
Genius_DSY <genius_dsy@foxmail.com>
2021-02-17 20:44:49 +0800

    :sparkles: feat:Sponsor启动类

---
+++ ad-service/ad-sponsor/src/main/java/cn/edu/cqupt/pikachu/ad/SponsorApplication.java
@@ -0,0 +1,27 @@
+package cn.edu.cqupt.pikachu.ad;
+
+import cn.edu.cqupt.pikachu.ad.dao.AdUserRepository;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+import javax.annotation.Resource;
+
+/**
+ * @author :DengSiYuan
+ * @date :2021/1/30 21:58
+ * @desc : 启动入口
+ */
+@EnableFeignClients
+@EnableCircuitBreaker
+@EnableEurekaClient
+@SpringBootApplication
+public class SponsorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SponsorApplication.class);
+    }
+
+}

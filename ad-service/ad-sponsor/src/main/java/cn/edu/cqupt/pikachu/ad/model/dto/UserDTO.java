675282ce2c35a063fa590f2a05c1a1bbf438a79d
Genius_DSY <genius_dsy@foxmail.com>
2021-02-24 18:05:25 +0800

    :sparkles:feat: 用户DTO

---
+++ ad-service/ad-sponsor/src/main/java/cn/edu/cqupt/pikachu/ad/model/dto/UserDTO.java
@@ -0,0 +1,28 @@
+package cn.edu.cqupt.pikachu.ad.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.apache.commons.lang.StringUtils;
+
+/**
+ * @author :DengSiYuan
+ * @date :2021/2/5 15:18
+ * @desc : 用户信息传入实体数据
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserDTO {
+
+    /**
+     * 用户名称
+     */
+    private String username;
+
+    public boolean validate() {
+        return StringUtils.isNotEmpty(username);
+    }
+
+
+}

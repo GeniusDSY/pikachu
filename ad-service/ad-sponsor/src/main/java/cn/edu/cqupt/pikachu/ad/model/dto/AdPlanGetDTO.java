12adfe15f4c854dd3c1b40c77543cd28d7a63d9a
Genius_DSY <genius_dsy@foxmail.com>
2021-02-24 17:59:07 +0800

    :sparkles:feat: 获取广告计划DTO

---
+++ ad-service/ad-sponsor/src/main/java/cn/edu/cqupt/pikachu/ad/model/dto/AdPlanGetDTO.java
@@ -0,0 +1,34 @@
+package cn.edu.cqupt.pikachu.ad.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.util.CollectionUtils;
+
+import java.util.List;
+
+/**
+ * @author :DengSiYuan
+ * @date :2021/2/6 12:44
+ * @desc : 批量获取推广计划的数据请求
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AdPlanGetDTO {
+
+    /**
+     * 用户Id
+     */
+    private Long userId;
+
+    /**
+     * 推广计划Id列表
+     */
+    private List<Long> ids;
+
+    public boolean validate() {
+        return  null != userId && !CollectionUtils.isEmpty(ids);
+    }
+
+}

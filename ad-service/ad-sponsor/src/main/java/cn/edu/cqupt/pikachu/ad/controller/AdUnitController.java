d18c9843558ec21ef293d5f5bd2f639bfd5bb141
Genius_DSY <genius_dsy@foxmail.com>
2021-03-03 22:11:33 +0800

    :sparkles:feat:广告单元Controller

---
+++ ad-service/ad-sponsor/src/main/java/cn/edu/cqupt/pikachu/ad/controller/AdUnitController.java
@@ -0,0 +1,58 @@
+package cn.edu.cqupt.pikachu.ad.controller;
+
+import cn.edu.cqupt.pikachu.ad.exception.AdException;
+import cn.edu.cqupt.pikachu.ad.model.dto.*;
+import cn.edu.cqupt.pikachu.ad.model.vo.*;
+import cn.edu.cqupt.pikachu.ad.service.IAdUnitService;
+import com.alibaba.fastjson.JSON;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+/**
+ * @author :DengSiYuan
+ * @date :2021/2/24 18:08
+ * @desc : 推广单元Controller
+ */
+@Slf4j
+@RestController
+@RequestMapping("adUnit")
+public class AdUnitController {
+
+    @Resource
+    private IAdUnitService adUnitService;
+
+    @PostMapping("create/unit")
+    public AdUnitVO createUnit(AdUnitDTO adUnitDTO) throws AdException{
+        log.info("AdUnitController createUnit -> {}", JSON.toJSONString(adUnitDTO));
+        return adUnitService.createUnit(adUnitDTO);
+    }
+
+    @PostMapping("create/unitKeyword")
+    public AdUnitKeywordVO createUnitKeyword(AdUnitKeywordDTO adUnitKeywordDTO) throws AdException {
+        log.info("AdUnitController createUnitKeyword -> {}", JSON.toJSONString(adUnitKeywordDTO));
+        return adUnitService.createUnitKeyword(adUnitKeywordDTO);
+    }
+
+    @PostMapping("create/unitIt")
+    public AdUnitItVO createUnitIt(AdUnitItDTO adUnitItDTO) throws AdException {
+        log.info("AdUnitController createUnitIt -> {}", JSON.toJSONString(adUnitItDTO));
+        return adUnitService.createUnitIt(adUnitItDTO);
+    }
+
+    @PostMapping("create/unitDistrict")
+    public AdUnitDistrictVO createUnitDistrict(AdUnitDistrictDTO adUnitDistrictDTO) throws AdException {
+        log.info("AdUnitController createUnitDistrict -> {}", JSON.toJSONString(adUnitDistrictDTO));
+        return adUnitService.createUnitDistrict(adUnitDistrictDTO);
+    }
+
+    @PostMapping("create/creativeUnit")
+    public CreativeUnitVO createCreativeUnit(CreativeUnitDTO creativeUnitDTO) throws AdException {
+        log.info("AdUnitController createCreativeUnit -> {}", JSON.toJSONString(creativeUnitDTO));
+        return adUnitService.createCreativeUnit(creativeUnitDTO);
+    }
+
+}

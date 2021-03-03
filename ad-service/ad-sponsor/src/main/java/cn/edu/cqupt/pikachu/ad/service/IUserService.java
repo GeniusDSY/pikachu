b692089a25b8bfc269eb9f672d153b9cb1abde9f
Genius_DSY <genius_dsy@foxmail.com>
2021-03-03 22:13:27 +0800

    :sparkles:feat:广告系统用户服务接口

---
+++ ad-service/ad-sponsor/src/main/java/cn/edu/cqupt/pikachu/ad/service/IUserService.java
@@ -0,0 +1,33 @@
+package cn.edu.cqupt.pikachu.ad.service;
+
+import cn.edu.cqupt.pikachu.ad.exception.AdException;
+import cn.edu.cqupt.pikachu.ad.model.dto.UserDTO;
+import cn.edu.cqupt.pikachu.ad.model.vo.UserVO;
+import cn.edu.cqupt.pikachu.ad.model.vo.response.Response;
+
+/**
+ * @author :DengSiYuan
+ * @date :2021/2/4 22:37
+ * @desc : 用户服务接口
+ */
+public interface IUserService {
+
+    /**
+     * 创建新用户
+     *
+     * @param userDTO 用户信息
+     * @return 用户展示信息
+     * @throws AdException 自定义异常捕获
+     */
+    UserVO createUser(UserDTO userDTO) throws AdException;
+
+    /**
+     * 更新用户资料
+     *
+     * @param userDTO 用户信息
+     * @return 用户展示信息
+     * @throws AdException 自定义异常捕获
+     */
+    UserVO updateUser(UserDTO userDTO) throws AdException;
+
+}

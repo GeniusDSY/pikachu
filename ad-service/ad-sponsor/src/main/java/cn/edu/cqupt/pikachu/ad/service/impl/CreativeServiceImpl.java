24b474e40d5e2892fe3cfe3d2d08bc2d4d731da3
Genius_DSY <genius_dsy@foxmail.com>
2021-03-08 10:17:39 +0800

    :sparkles: feat:广告系统创意服务CreativeServiceImpl

---
+++ ad-service/ad-sponsor/src/main/java/cn/edu/cqupt/pikachu/ad/service/impl/CreativeServiceImpl.java
@@ -0,0 +1,59 @@
+package cn.edu.cqupt.pikachu.ad.service.impl;
+
+import cn.edu.cqupt.pikachu.ad.constants.enums.ResultStatus;
+import cn.edu.cqupt.pikachu.ad.dao.AdUserRepository;
+import cn.edu.cqupt.pikachu.ad.dao.CreativeRepository;
+import cn.edu.cqupt.pikachu.ad.exception.AdException;
+import cn.edu.cqupt.pikachu.ad.model.dto.CreativeDTO;
+import cn.edu.cqupt.pikachu.ad.model.entity.AdUser;
+import cn.edu.cqupt.pikachu.ad.model.entity.Creative;
+import cn.edu.cqupt.pikachu.ad.model.vo.CreativeVO;
+import cn.edu.cqupt.pikachu.ad.service.ICreativeService;
+import cn.edu.cqupt.pikachu.ad.utils.ConvertUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.Optional;
+
+/**
+ * @author :DengSiYuan
+ * @date :2021/2/17 9:56
+ * @desc : 创意服务实现
+ */
+@Slf4j
+@Service
+public class CreativeServiceImpl implements ICreativeService {
+
+    @Resource
+    private CreativeRepository creativeRepository;
+
+    @Resource
+    private AdUserRepository userRepository;
+
+    /**
+     * 创建创意
+     *
+     * @param creativeDTO 创意传入数据
+     * @return 创意展示数据
+     */
+    @Override
+    public CreativeVO createCreative(CreativeDTO creativeDTO) throws AdException{
+
+        if (!creativeDTO.createValidate()) {
+            throw new AdException(ResultStatus.REQUEST_PARAM_ERROR);
+        }
+
+        // 判断用户是否存在
+        Optional<AdUser> user = userRepository.findById(creativeDTO.getUserId());
+        if (!user.isPresent()) {
+            throw new AdException(ResultStatus.USER_NOT_EXISTED);
+        }
+
+        // 创建新的创意存入
+        Creative creative = creativeRepository.save(ConvertUtils.creativeDTO2Creative(creativeDTO));
+
+        return new CreativeVO(creative.getId(), creative.getName());
+    }
+
+}

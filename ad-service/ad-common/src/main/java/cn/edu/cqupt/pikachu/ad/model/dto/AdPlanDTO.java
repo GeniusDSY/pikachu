01bea9864d17c4bbf1ce5e4ea06004788a945bd0
Genius_DSY <genius_dsy@foxmail.com>
2021-03-09 16:59:41 +0800

    :art: style: 修改引入依赖的版本

--- ad-service/ad-common/src/main/java/cn/edu/cqupt/pikachu/ad/model/dto/AdPlanDTO.java
+++ ad-service/ad-common/src/main/java/cn/edu/cqupt/pikachu/ad/model/dto/AdPlanDTO.java
@@ -3,7 +3,7 @@ package cn.edu.cqupt.pikachu.ad.model.dto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 
 /**
  * @author :DengSiYuan

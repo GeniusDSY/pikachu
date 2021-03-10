f5f572bb35b77dbc67daf1cc2705998a46fd491b
Genius_DSY <genius_dsy@foxmail.com>
2021-03-10 20:20:54 +0800

    :sparkles: feat:调用广告投放系统Client

---
+++ ad-service/ad-search/src/main/java/cn/edu/cqupt/pikachu/ad/client/SponsorClient.java
@@ -0,0 +1,28 @@
+package cn.edu.cqupt.pikachu.ad.client;
+
+import cn.edu.cqupt.pikachu.ad.model.dto.AdPlanGetDTO;
+import cn.edu.cqupt.pikachu.ad.model.vo.AdPlanVO;
+import cn.edu.cqupt.pikachu.ad.model.vo.response.Response;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
+
+/**
+ * @author :DengSiYuan
+ * @date :2021/3/9 14:14
+ * @desc : 投放客户端
+ */
+@FeignClient(value = "eureka-client-ad-sponsor", fallback = SponsorClientHystrix.class)
+public interface SponsorClient {
+
+    /**
+     * 获取广告投放系统获取广告投放计划
+     *
+     * @param adPlanGetDTO 广告计划获取请求数据
+     * @return 广告投放计划信息列表
+     */
+    @PostMapping("/ad-sponsor/adPlan/get")
+    Response<List<AdPlanVO>> getAdPlans(AdPlanGetDTO adPlanGetDTO);
+
+}

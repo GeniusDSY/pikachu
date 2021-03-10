b96c12ff0ab059a1e407c302deb400d3871aebd9
Genius_DSY <genius_dsy@foxmail.com>
2021-03-10 20:22:44 +0800

    :sparkles: feat:广告检索系统索引接口

---
+++ ad-service/ad-search/src/main/java/cn/edu/cqupt/pikachu/ad/index/IndexAware.java
@@ -0,0 +1,41 @@
+package cn.edu.cqupt.pikachu.ad.index;
+
+/**
+ * @author :DengSiYuan
+ * @date :2021/3/9 16:42
+ * @desc : 索引
+ */
+public interface IndexAware<K, V> {
+
+    /**
+     * 获取索引
+     *
+     * @param key 索引的键
+     * @return 索引值
+     */
+    V get(K key);
+
+    /**
+     * 添加索引
+     *
+     * @param key 索引的键
+     * @param value 索引的值
+     */
+    void add(K key, V value);
+
+    /**
+     * 更新索引
+     *
+     * @param key 索引的键
+     * @param value 索引的值
+     */
+    void update(K key, V value);
+
+    /**
+     * 删除索引
+     *
+     * @param key 索引的键
+     */
+    void delete(K key, V value);
+
+}

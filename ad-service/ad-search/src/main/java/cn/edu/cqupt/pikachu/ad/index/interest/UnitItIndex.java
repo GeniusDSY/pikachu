8899144c180c016bc2596fd71babb2df2b91a61c
Genius_DSY <genius_dsy@foxmail.com>
2021-03-12 12:18:40 +0800

    :sparkles: feat:广告单元兴趣爱好影响因素索引

---
+++ ad-service/ad-search/src/main/java/cn/edu/cqupt/pikachu/ad/index/interest/UnitItIndex.java
@@ -0,0 +1,119 @@
+package cn.edu.cqupt.pikachu.ad.index.interest;
+
+import cn.edu.cqupt.pikachu.ad.index.IndexAware;
+import cn.edu.cqupt.pikachu.ad.utils.CommonUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.collections4.CollectionUtils;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentSkipListSet;
+
+/**
+ * @author :DengSiYuan
+ * @date :2021/3/10 19:39
+ * @desc : 推广单元兴趣索引
+ */
+@Slf4j
+@Component
+public class UnitItIndex implements IndexAware<String, Set<Long>> {
+
+    /**
+     * {itTag, adUnitId}
+     */
+    private static Map<String, Set<Long>> itUnitMap;
+
+    /**
+     * {adUnitId, itTag}
+     */
+    private static Map<Long, Set<String>> unitItMap;
+
+    static {
+        itUnitMap = new ConcurrentHashMap<>();
+        unitItMap = new ConcurrentHashMap<>();
+    }
+
+    /**
+     * 获取索引
+     *
+     * @param key 索引的键
+     * @return 索引值
+     */
+    @Override
+    public Set<Long> get(String key) {
+        return itUnitMap.get(key);
+    }
+
+    /**
+     * 添加索引
+     *
+     * @param key   索引的键
+     * @param value 索引的值
+     */
+    @Override
+    public void add(String key, Set<Long> value) {
+
+        Set<Long> unitIds = CommonUtils.getorCreate(key, itUnitMap, ConcurrentSkipListSet::new);
+        unitIds.addAll(value);
+
+        for (Long unitId : value) {
+            Set<String> its = CommonUtils.getorCreate(unitId, unitItMap, ConcurrentSkipListSet::new);
+            its.add(key);
+        }
+
+        log.info("UnitItIndex add -> key:{}, value:{}", key, value,
+                "itUnitMap:{}", itUnitMap,
+                "unitItMap:{}", unitItMap);
+
+    }
+
+    /**
+     * 更新索引
+     *
+     * @param key   索引的键
+     * @param value 索引的值
+     */
+    @Override
+    public void update(String key, Set<Long> value) {
+
+        log.error("UnitItIndex update -> {}",
+                "keyword index cannot support update,You can do this by deleting it first and adding it later");
+    }
+
+    /**
+     * 删除索引
+     *
+     * @param key   索引的键
+     * @param value
+     */
+    @Override
+    public void delete(String key, Set<Long> value) {
+
+        Set<Long> unitIds = CommonUtils.getorCreate(key, itUnitMap, ConcurrentSkipListSet::new);
+        unitIds.removeAll(value);
+
+        for (Long unitId : value) {
+            Set<String> ItTagSet = CommonUtils.getorCreate(unitId, unitItMap, ConcurrentSkipListSet::new);
+            ItTagSet.remove(key);
+        }
+
+        log.info("UnitItIndex delete -> key:{}, value:{}", key, value,
+                "itUnitMap:{}", itUnitMap,
+                "unitItMap:{}", unitItMap);
+    }
+
+    public boolean match(Long unitId, List<String> itTags) {
+
+        if (unitItMap.containsKey(unitId) && CollectionUtils.isNotEmpty(unitItMap.get(unitId))) {
+            Set<String> unitKeywords = unitItMap.get(unitId);
+
+            return CollectionUtils.isSubCollection(itTags, unitKeywords);
+        }
+
+        return false;
+    }
+
+}

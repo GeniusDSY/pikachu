99b0e1e328017988d502e8512388397b935d7719
Genius_DSY <genius_dsy@foxmail.com>
2021-03-18 11:37:31 +0800

    :sparkles: feat:广告关键字数据表

---
+++ ad-service/ad-common/src/main/java/cn/edu/cqupt/pikachu/ad/dump/table/AdKeywordTable.java
@@ -0,0 +1,26 @@
+package cn.edu.cqupt.pikachu.ad.dump.table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author :DengSiYuan
+ * @date :2021/3/18 10:51
+ * @desc : 广告关键词数据表
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AdKeywordTable {
+
+    /**
+     * 推广单元Id
+     */
+    private Long unitId;
+
+    /**
+     * 关键词
+     */
+    private String keyword;
+}

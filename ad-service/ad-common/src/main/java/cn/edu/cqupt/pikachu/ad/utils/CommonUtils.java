8e575afb1eda974561de483ec10cd94cc24435b8
Genius_DSY <genius_dsy@foxmail.com>
2021-03-22 20:46:14 +0800

    :sparkles: feat:新增工具方法字符串拼接

--- ad-service/ad-common/src/main/java/cn/edu/cqupt/pikachu/ad/utils/CommonUtils.java
+++ ad-service/ad-common/src/main/java/cn/edu/cqupt/pikachu/ad/utils/CommonUtils.java
@@ -67,4 +67,20 @@ public class CommonUtils {
     public static <K, V> V getorCreate(K key, Map<K, V> map, Supplier<V> factory) {
         return map.computeIfAbsent(key, k -> factory.get());
     }
+
+    /**
+     * 字符串拼接（”-“）
+     *
+     * @param args 参数
+     * @return 拼接结果
+     */
+    public static String stringConcat(String... args) {
+
+        StringBuilder result = new StringBuilder();
+        for (String arg : args) {
+            result.append(arg);
+            result.append("-");
+        }
+        return result.deleteCharAt(result.length()).toString();
+    }
 }

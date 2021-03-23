73240acc6c6f775a53abc4f9b4b0efdb4611b40c
Genius_DSY <genius_dsy@foxmail.com>
2021-03-23 13:51:38 +0800

    :sparkles: feat:广告单元关键字影响因素DAO

---
+++ ad-service/ad-sponsor/src/main/java/cn/edu/cqupt/pikachu/ad/dao/unit_condition/AdUnitKeywordRepository.java
@@ -0,0 +1,12 @@
+package cn.edu.cqupt.pikachu.ad.dao.unit_condition;
+
+import cn.edu.cqupt.pikachu.ad.model.entity.unit_condition.AdUnitKeyword;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * @author :DengSiYuan
+ * @date :2021/2/4 21:24
+ * @desc :
+ */
+public interface AdUnitKeywordRepository extends JpaRepository<AdUnitKeyword, Long> {
+}

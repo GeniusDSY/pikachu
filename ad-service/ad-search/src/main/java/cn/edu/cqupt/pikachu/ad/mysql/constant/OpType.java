322468a01a8ae26d3bcdee735f0b40d6bb1f6fc1
Genius_DSY <genius_dsy@foxmail.com>
2021-03-23 13:54:22 +0800

    :sparkles: feat:操作类型（增删改查）

---
+++ ad-service/ad-search/src/main/java/cn/edu/cqupt/pikachu/ad/mysql/constant/OpType.java
@@ -0,0 +1,16 @@
+package cn.edu.cqupt.pikachu.ad.mysql.constant;
+
+/**
+ * @author :DengSiYuan
+ * @date :2021/3/22 14:40
+ * @desc : 操作类型
+ */
+public enum OpType {
+
+    ADD,
+    UPDATE,
+    DELETE,
+    OTHER,
+    ;
+
+}

b10a9606ae1a63a72ac08ab8c6e822152f92f0b1
Genius_DSY <genius_dsy@foxmail.com>
2021-03-23 13:51:20 +0800

    :sparkles: feat:广告单元兴趣爱好影响因素DAO

---
+++ ad-service/ad-sponsor/src/main/java/cn/edu/cqupt/pikachu/ad/dao/unit_condition/AdUnitItRepository.java
@@ -0,0 +1,12 @@
+package cn.edu.cqupt.pikachu.ad.dao.unit_condition;
+
+import cn.edu.cqupt.pikachu.ad.model.entity.unit_condition.AdUnitIt;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * @author :DengSiYuan
+ * @date :2021/2/4 21:45
+ * @desc : 广告推广单元兴趣数据查询
+ */
+public interface AdUnitItRepository extends JpaRepository<AdUnitIt, Long> {
+}

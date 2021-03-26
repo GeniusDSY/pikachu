32e35203fe83bf50ef113a715a0421f80c9cad5b
Genius_DSY <genius_dsy@foxmail.com>
2021-03-26 21:48:42 +0800

    :bug: bug:修复工具模块打包时找不到main入口的异常

---
+++ ad-service/ad-common/src/main/java/cn/edu/cqupt/pikachu/ad/CommonApplication.java
@@ -0,0 +1,19 @@
+package cn.edu.cqupt.pikachu.ad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author :DengSiYuan
+ * @date :2021/2/28 0:12
+ * @desc :
+ */
+@SpringBootApplication
+public class CommonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommonApplication.class, args);
+    }
+
+
+}

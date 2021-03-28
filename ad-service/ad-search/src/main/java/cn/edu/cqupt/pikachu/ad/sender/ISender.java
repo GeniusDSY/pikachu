21d6a143f1974ef1963cecf84d45df98d6396a27
Genius_DSY <genius_dsy@foxmail.com>
2021-03-28 20:01:14 +0800

    :sparkles: feat:增量索引推送接口

---
+++ ad-service/ad-search/src/main/java/cn/edu/cqupt/pikachu/ad/sender/ISender.java
@@ -0,0 +1,18 @@
+package cn.edu.cqupt.pikachu.ad.sender;
+
+import cn.edu.cqupt.pikachu.ad.mysql.dto.MySqlRowData;
+
+/**
+ * @author :DengSiYuan
+ * @date :2021/3/28 18:16
+ * @desc : 增量数据投递接口
+ */
+public interface ISender {
+
+    /**
+     * 增连数据投递
+     *
+     * @param rowData 投递数据
+     */
+    void sender(MySqlRowData rowData);
+}

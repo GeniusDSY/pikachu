45c3a706a9f618e33255d2f59411ae2b50d307e8
Genius_DSY <genius_dsy@foxmail.com>
2021-03-28 18:12:55 +0800

    :sparkles: feat:增加事件操作类型转换为操作类型的方法

--- ad-service/ad-search/src/main/java/cn/edu/cqupt/pikachu/ad/mysql/constant/OpType.java
+++ ad-service/ad-search/src/main/java/cn/edu/cqupt/pikachu/ad/mysql/constant/OpType.java
@@ -1,5 +1,7 @@
 package cn.edu.cqupt.pikachu.ad.mysql.constant;
 
+import com.github.shyiko.mysql.binlog.event.EventType;
+
 /**
  * @author :DengSiYuan
  * @date :2021/3/22 14:40
@@ -7,10 +9,42 @@ package cn.edu.cqupt.pikachu.ad.mysql.constant;
  */
 public enum OpType {
 
+    /**
+     * Insert操作
+     */
     ADD,
+    /**
+     * 更新操作
+     */
     UPDATE,
+    /**
+     * 删除操作
+     */
     DELETE,
+    /**
+     * 其他操作
+     */
     OTHER,
     ;
+
+    /**
+     * 事件操作类型转换
+     *
+     * @param eventType 事件类型
+     * @return 操作类型
+     */
+    public static OpType to(EventType eventType) {
+
+        switch (eventType) {
+            case EXT_WRITE_ROWS:
+                return ADD;
+            case EXT_UPDATE_ROWS:
+                return UPDATE;
+            case EXT_DELETE_ROWS:
+                return DELETE;
+            default:
+                return OTHER;
+        }
+    }
 
 }

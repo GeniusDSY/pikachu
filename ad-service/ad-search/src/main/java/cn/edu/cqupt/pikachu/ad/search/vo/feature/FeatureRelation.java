fb0282075b59810a9a7fcc9927e5a62c399b9cdc
Genius_DSY <genius_dsy@foxmail.com>
2021-03-29 21:12:59 +0800

    :sparkles: feat:匹配信息关联状态

---
+++ ad-service/ad-search/src/main/java/cn/edu/cqupt/pikachu/ad/search/vo/feature/FeatureRelation.java
@@ -0,0 +1,20 @@
+package cn.edu.cqupt.pikachu.ad.search.vo.feature;
+
+/**
+ * @author :DengSiYuan
+ * @date :2021/3/29 20:10
+ * @desc : 匹配关系
+ */
+public enum FeatureRelation {
+
+    /**
+     * 且
+     */
+    AND,
+
+    /**
+     * 或
+     */
+    OR,
+    ;
+}

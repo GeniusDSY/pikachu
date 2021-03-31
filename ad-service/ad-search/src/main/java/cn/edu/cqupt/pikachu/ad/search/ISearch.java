25592cf0688b41713b536f91fe73e7803bf3f3d7
Genius_DSY <genius_dsy@foxmail.com>
2021-03-31 22:28:01 +0800

    :sparkles: feat:广告检索接口

---
+++ ad-service/ad-search/src/main/java/cn/edu/cqupt/pikachu/ad/search/ISearch.java
@@ -0,0 +1,20 @@
+package cn.edu.cqupt.pikachu.ad.search;
+
+import cn.edu.cqupt.pikachu.ad.search.vo.SearchRequest;
+import cn.edu.cqupt.pikachu.ad.search.vo.SearchResponse;
+
+/**
+ * @author :DengSiYuan
+ * @date :2021/3/29 17:57
+ * @desc : 检索服务
+ */
+public interface ISearch {
+
+    /**
+     * 获取广告
+     *
+     * @param request 检索请求
+     * @return 检索响应
+     */
+    SearchResponse fetchAds(SearchRequest request);
+}

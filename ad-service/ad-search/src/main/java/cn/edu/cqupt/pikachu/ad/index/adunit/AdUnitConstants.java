31ed11e5f3c145599a2d58314347d3ad046e837f
Genius_DSY <genius_dsy@foxmail.com>
2021-03-31 22:24:47 +0800

    :sparkles: feat:å广告单元常量类

---
+++ ad-service/ad-search/src/main/java/cn/edu/cqupt/pikachu/ad/index/adunit/AdUnitConstants.java
@@ -0,0 +1,40 @@
+package cn.edu.cqupt.pikachu.ad.index.adunit;
+
+/**
+ * @author :DengSiYuan
+ * @date :2021/3/30 13:24
+ * @desc : 广告单元常量类
+ */
+public class AdUnitConstants {
+
+    /**
+     * 流量类型
+     */
+    public static class POSITION_TYPE {
+
+        /**
+         * 开屏
+         */
+        public static final int KAIPING = 1;
+
+        /**
+         * 贴片
+         */
+        public static final int TIEPIAN = 2;
+
+        /**
+         * 中部贴片
+         */
+        public static final int TIEPIAN_MIDDLE = 4;
+
+        /**
+         * 暂停贴片
+         */
+        public static final int TIEPIAN_PAUSE = 8;
+
+        /**
+         * 贴片后端
+         */
+        public static final int TIEPIAN_POST = 16;
+    }
+}

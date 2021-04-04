0fe741b730d9a5743b3245e009b1005647637bbb
Genius_DSY <genius_dsy@foxmail.com>
2021-04-04 19:58:57 +0800

    :art: refactor:修改UnitKeywordTable类名为AdUnitKeywordTable

---
+++ ad-service/ad-common/src/main/java/cn/edu/cqupt/pikachu/ad/dump/DConstant.java
@@ -0,0 +1,49 @@
+package cn.edu.cqupt.pikachu.ad.dump;
+
+/**
+ * @author :DengSiYuan
+ * @date :2021/3/15 20:30
+ * @desc : 全量数据
+ */
+public class DConstant {
+
+    /**
+     * 存储地址
+     */
+    public static final String DATA_ROOT_STR = "E:\\Data\\Data from Background\\JAVA\\My Projects\\pikachu\\ad-service\\ad-export\\src\\main\\resources\\mysql_data\\";
+
+    /**
+     * ad_plan表数据的存储文件名
+     */
+    public static final String AD_PLAN = "ad_plan.data";
+
+    /**
+     * ad_unit
+     */
+    public static final String AD_UNIT = "ad_unit.data";
+
+    /**
+     * ad_creative
+     */
+    public static final String AD_CREATIVE = "ad_creative.data";
+
+    /**
+     * ad_creative_unit
+     */
+    public static final String AD_CREATIVE_UNIT = "ad_creative_unit.data";
+
+    /**
+     * ad_unit_it
+     */
+    public static final String AD_UNIT_IT = "ad_unit_it.data";
+
+    /**
+     * ad_unit_district
+     */
+    public static final String AD_UNIT_DISTRICT = "ad_unit_district.data";
+
+    /**
+     * ad_unit_keyword
+     */
+    public static final String AD_UNIT_KEYWORD = "ad_unit_keyword.data";
+}

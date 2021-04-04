a83771d3e169a11a2ce942d38c017dfea02ff078
Genius_DSY <genius_dsy@foxmail.com>
2021-04-04 20:07:08 +0800

    :sparkles: feat:修改响应的格式

--- ad-service/ad-search/src/main/java/cn/edu/cqupt/pikachu/ad/controller/SearchController.java
+++ ad-service/ad-search/src/main/java/cn/edu/cqupt/pikachu/ad/controller/SearchController.java
@@ -10,7 +10,9 @@ import cn.edu.cqupt.pikachu.ad.search.vo.SearchRequest;
 import cn.edu.cqupt.pikachu.ad.search.vo.SearchResponse;
 import com.alibaba.fastjson.JSON;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
@@ -29,32 +31,34 @@ public class SearchController {
     @Resource
     private RestTemplate restTemplate;
 
-    @Resource
-    private SponsorClient sponsorClient;
+    private final SponsorClient sponsorClient;
 
-    @Resource
-    private ISearch search;
+    private final ISearch search;
+
+    @Autowired
+    public SearchController(ISearch search, SponsorClient sponsorClient) {
+        this.search = search;
+        this.sponsorClient = sponsorClient;
+    }
 
     @SuppressWarnings("all")
-    @IgnoreResponseAdvice
     @PostMapping("getAdPlansByRibbon")
-    public Response<List<AdPlanVO>> getAdPlansByRebbion(AdPlanGetDTO adPlanGetDTO) {
+    public Response<List<AdPlanVO>> getAdPlansByRebbion(@RequestBody AdPlanGetDTO adPlanGetDTO) {
         log.info("ad-search: SearchController getAdPlansByRebbion -> {}", JSON.toJSONString(adPlanGetDTO));
         return restTemplate.postForEntity(
-                "http://eureka-client-ad-sponsor/ad-sponsor/adPlan/get",
+                "http://ad-sponsor/ad-sponsor/adPlan/get",
                 adPlanGetDTO,
                 Response.class).getBody();
     }
 
-    @IgnoreResponseAdvice
     @PostMapping("getAdPlans")
-    public Response<List<AdPlanVO>> getAdPlans(AdPlanGetDTO adPlanGetDTO) {
+    public Response<List<AdPlanVO>> getAdPlans(@RequestBody AdPlanGetDTO adPlanGetDTO) {
         log.info("ad-search: SearchController getAdPlans -> {}", JSON.toJSONString(adPlanGetDTO));
         return sponsorClient.getAdPlans(adPlanGetDTO);
     }
 
     @PostMapping("fetchAds")
-    public SearchResponse fetchAds(SearchRequest request) {
+    public SearchResponse fetchAds(@RequestBody SearchRequest request) {
         log.info("ad-search: SearchController fetchAds -> {}", JSON.toJSONString(request));
         return search.fetchAds(request);
 

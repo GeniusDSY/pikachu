9e50e325852cddf4730e3245aa0e642233a59cf1
Genius_DSY <genius_dsy@foxmail.com>
2021-04-04 20:01:45 +0800

    :sparkles: feat:监控面板启动类

---
+++ ad-service/ad-dashboard/src/main/java/cn/edu/cqupt/pikachu/ad/DashboardApplication.java
@@ -0,0 +1,21 @@
+package cn.edu.cqupt.pikachu.ad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+/**
+ * @author :DengSiYuan
+ * @date :2021/4/2 14:13
+ * @desc : 监控面板启动程序
+ */
+@EnableEurekaClient
+@SpringBootApplication
+@EnableHystrixDashboard
+public class DashboardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DashboardApplication.class, args);
+    }
+}

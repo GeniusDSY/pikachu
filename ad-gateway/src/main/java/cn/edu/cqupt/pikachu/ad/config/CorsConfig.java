c0cdf76250e439ae6311503b3a87d061919b3802
Genius_DSY <genius_dsy@foxmail.com>
2021-04-04 19:53:38 +0800

    :sparkles: feat:增加跨域配置

---
+++ ad-gateway/src/main/java/cn/edu/cqupt/pikachu/ad/config/CorsConfig.java
@@ -0,0 +1,35 @@
+package cn.edu.cqupt.pikachu.ad.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.Collections;
+
+/**
+ * @author :DengSiYuan
+ * @date :2019/6/14 9:21
+ * @desc : 跨域配置
+ */
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsFilter corsFilter(){
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        final CorsConfiguration config = new CorsConfiguration();
+
+        config.setAllowCredentials(true);
+        config.setAllowedOrigins(Collections.singletonList("*"));
+        config.setAllowedHeaders(Collections.singletonList("*"));
+        config.setAllowedMethods(Collections.singletonList("*"));
+        config.setMaxAge(300L);
+
+        source.registerCorsConfiguration("/**",config);
+
+        return new CorsFilter(source);
+    }
+
+}

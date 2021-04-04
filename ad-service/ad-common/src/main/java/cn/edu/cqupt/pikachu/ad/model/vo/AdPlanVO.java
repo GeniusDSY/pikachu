517031c807764b1a04856beebe6bd2284db4766e
Genius_DSY <genius_dsy@foxmail.com>
2021-04-04 20:37:03 +0800

    :spakrles: feat:修改参数顺序，保证请求的正确

--- ad-service/ad-common/src/main/java/cn/edu/cqupt/pikachu/ad/model/vo/AdPlanVO.java
+++ ad-service/ad-common/src/main/java/cn/edu/cqupt/pikachu/ad/model/vo/AdPlanVO.java
@@ -4,6 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.Date;
+
 /**
  * @author :DengSiYuan
  * @date :2021/2/6 11:44
@@ -23,4 +25,30 @@ public class AdPlanVO {
      * 推广计划名称
      */
     private String planName;
+
+
+    /**
+     * 推广计划状态
+     */
+    private Integer planStatus;
+
+    /**
+     * 推广计划开始时间
+     */
+    private Date startDate;
+
+    /**
+     * 推广计划结束时间
+     */
+    private Date endDate;
+
+    /**
+     * 推广计划创建时间
+     */
+    private Date createTime;
+
+    /**
+     * 推广计划更新时间
+     */
+    private Date updateTime;
 }

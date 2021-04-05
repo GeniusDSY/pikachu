9fcaa5d6299688bf7b7d3abf5324cc56ed74b2fc
Genius_DSY <genius_dsy@foxmail.com>
2021-04-05 22:23:32 +0800

    :sparkles: feat:增加监控节点

--- ad-service/ad-sponsor/src/main/java/cn/edu/cqupt/pikachu/ad/SponsorApplication.java
+++ ad-service/ad-sponsor/src/main/java/cn/edu/cqupt/pikachu/ad/SponsorApplication.java
@@ -1,10 +1,14 @@
 package cn.edu.cqupt.pikachu.ad;
 
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 
 /**
  * @author :DengSiYuan
@@ -19,6 +23,16 @@ public class SponsorApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SponsorApplication.class);
+    }
+
+    @Bean
+    public ServletRegistrationBean getServlet() {
+        HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean(streamServlet);
+        registrationBean.setLoadOnStartup(1);
+        registrationBean.addUrlMappings("/hystrix.stream");
+        registrationBean.setName("HystrixMetricsStreamServlet");
+        return registrationBean;
     }
 
 }

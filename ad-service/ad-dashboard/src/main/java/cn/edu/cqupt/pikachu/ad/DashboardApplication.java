67da87eda14a42899cf104558ce30f0ac848709d
Genius_DSY <genius_dsy@foxmail.com>
2021-04-05 22:22:46 +0800

    :sparkles: feat:增加集群监控注解

--- ad-service/ad-dashboard/src/main/java/cn/edu/cqupt/pikachu/ad/DashboardApplication.java
+++ ad-service/ad-dashboard/src/main/java/cn/edu/cqupt/pikachu/ad/DashboardApplication.java
@@ -4,12 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
 
 /**
  * @author :DengSiYuan
  * @date :2021/4/2 14:13
  * @desc : 监控面板启动程序
  */
+@EnableTurbine
 @EnableEurekaClient
 @SpringBootApplication
 @EnableHystrixDashboard
